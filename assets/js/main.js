c34b6461f875f884b12167b9f87e198d6360efcf
Ahmed khatab <ahmed.khatab@adamtravel.net>
2022-10-05 18:04:16 +0200

    full site upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+  toggleNav()
   $(".filter-button").click(function(){
     $(".filter-button").removeClass("active")
 
@@ -14,19 +14,63 @@ $(document).ready(function(){
     {
       $(".filter").not('.'+value).hide('3000');
       $('.filter').filter('.'+value).show('3000');
-        
     }
-  });  
+  });
+  $(".lang").click(function(){
+    if ($(this)[0].innerText === 'عربي') {
+      for (let index = 0; index < $(".lang").length; index++) {
+        $(".lang")[index].innerText = 'English'
+      }
+      // $("#lang1")[0].innerText = 'English'
+      document.body.style.direction = "rtl";
+    } else {
+      for (let index = 0; index < $(".lang").length; index++) {
+        $(".lang")[index].innerText = 'عربي'
+      }
+      document.body.style.direction = "ltr";
+    }
+  })
 });
 
 window.onscroll = function() {toggleNav()};
+window.addEventListener('resize', toggleNav());
 
 function toggleNav() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    document.getElementById("nav").style.visibility = "visible";
-  } else {
-    document.getElementById("nav").style.visibility = "hidden";
+  const nav = document.getElementById("nav")
+  if (nav) {
+    if ((document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) && document.body.clientWidth > 992) {
+      nav.style.visibility = "visible";
+    } else if (document.body.clientWidth <= 992) {
+      nav.style.visibility = "visible";
+    } else {
+      nav.style.visibility = "hidden";
+    }
   }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  function OTPInput() {
+    const inputs = document.querySelectorAll('#otp > *[id]');
+    for (let i = 0; i < inputs.length; i++) { 
+      inputs[i].addEventListener('keydown', function(event) {
+        if (event.key==="Backspace" ) {
+          inputs[i].value='' ;
+          if (i !==0) inputs[i - 1].focus();
+        } else {
+          if (i===inputs.length - 1 && inputs[i].value !=='' ) {
+            return true;
+          } else if (event.keyCode> 47 && event.keyCode < 58) {
+            inputs[i].value=event.key; if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault();
+          } else if (event.keyCode> 64 && event.keyCode < 91) {
+            inputs[i].value=String.fromCharCode(event.keyCode);
+            if (i !==inputs.length - 1) inputs[i + 1].focus();
+            event.preventDefault();
+          }
+        }
+      });
+    }
+  }
+  OTPInput();
+});
 
    
